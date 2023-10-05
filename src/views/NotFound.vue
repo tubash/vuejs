fa8c72acc7d9fab8ad6d2c4ca0b96bbb424806c2
Serj Tubash <serj.tubash@gmail.com>
2023-10-05 20:17:38 +0300

    fixes the layout

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,27 @@
 <template>
-
+    <div class="wrapper">
+        <h1>
+            <span class="invert">404</span> not found
+        </h1>
+    </div>
 </template>
+  
 <style scoped>
-
+  .wrapper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .invert {
+    background-color: #000;
+    color: #fff;
+    padding: 0 15px;
+    border-radius: 10px;
+  }
+  html,body {
+    height: 100%;
+    min-height: 100% !important;
+    margin: auto;
+  }  
 </style>
